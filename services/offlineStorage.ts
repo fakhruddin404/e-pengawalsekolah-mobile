98ac949e73a3a16524627b0c20572e209c070aa4
din-gigit <dinz4123@gmail.com>
2026-07-04 23:09:01 +0800

    uu

--- services/offlineStorage.ts
+++ services/offlineStorage.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { type RondaanMapPoint } from './rondaanService';
 
 export type OfflineScanEntry = {
